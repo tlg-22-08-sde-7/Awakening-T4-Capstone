517b30a5289955b608d2bf63c8f7cd5c8d10b507
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2022-12-21 09:36:01 -0800

    AB#1041: Text input added to directional grid

--- src/com/awakening/gui/app/GameManager.java
+++ src/com/awakening/gui/app/GameManager.java
@@ -52,72 +52,24 @@ public class GameManager {
         ImageIcon icon = new ImageIcon("resources/images/titleScreen.PNG");
         imageLabel = new JLabel(icon);
 
-        layoutManager.addGB(imageLabel, 0, 0, 2, 4, .4, .1);
+        layoutManager.addGB(imageLabel, 0, 0, 2, 4, .2, .1);
     }
 
     public static void populateTextGrid() {
         UI ui = new UI();
         Game gameClassLoad = new Game();
-        TextParser textParser = new TextParser();
 
         String displayGameInfo = ui.displayGameInfo(Game.player);
         GameHomePage.getHomePageTextArea().setText(displayGameInfo);
         GameHomePage.getHomePageTextArea().setFont(Awakening_Font.getNormalFont());
         GameHomePage.getHomePageTextArea().setForeground(Color.RED);
 
-        inputTextPanel = new JPanel();
-        inputTextPanel.setBounds(250, 500, 400, 200);
-        inputTextPanel.setBackground(Color.black);
-
-        inputTextField = new JTextField();
-        inputTextField.setPreferredSize(new Dimension(150, 40));
-        inputTextField.setFont(Awakening_Font.getNormalFont());
-
-        // when the player clicks on the text field, clear the existing entry
-        inputTextField.addFocusListener(new FocusListener() {
-            @Override
-            public void focusGained(FocusEvent e) {
-                inputTextField.setText("");
-            }
-
-            @Override
-            public void focusLost(FocusEvent e) {
-
-            }
-        });
-
-        inputTextSubmitButton = new JButton("Submit");
-        inputTextSubmitButton.setBackground(Color.black);
-        inputTextSubmitButton.setForeground(Color.lightGray);
-        inputTextSubmitButton.setFocusPainted(false);
-        inputTextSubmitButton.setFont(Awakening_Font.buttonSelectionFont());
-        inputTextSubmitButton.addActionListener(e -> {
-            String input = inputTextField.getText();
-            List<String> command = textParser.parseInput(input);
-            if (!command.get(0).equals("invalid")){
-                if (command.get(0).equalsIgnoreCase("help")) {
-                    //showImagePage("resources/images/help.PNG", "Return");
-                }
-                // add quit logic
-                else if (command.get(0).equalsIgnoreCase("quit")) {
-                    // TODO: exit page before exiting the program
-                    System.exit(0);
-                }
-                else{
-                    gameClassLoad.executeCommand(command);
-                }
-            }
-        });
-
-        inputTextPanel.add(inputTextField);
-        inputTextPanel.add(inputTextSubmitButton);
-
-        GameStart.getContainer().add(inputTextPanel);
         layoutManager.addGB(GameStart.getContainer(), 0, 4, 3, 4, .7, .5);
     }
 
     public static void populateDirectionalGrid() {
         GridBagConstraints constraints = new GridBagConstraints();
+        TextParser textParser = new TextParser();
         constraints.fill = GridBagConstraints.BOTH;
 
         // Create JPanel for Directionals with LayoutManager
@@ -170,13 +122,62 @@ public class GameManager {
         });
         directionalPanel.add(west, constraints);
 
+        inputTextPanel = new JPanel();
+        inputTextPanel.setBackground(Color.black);
 
+        inputTextField = new JTextField();
+        inputTextField.setPreferredSize(new Dimension(150, 40));
+        inputTextField.setFont(Awakening_Font.getNormalFont());
+
+        // when the player clicks on the text field, clear the existing entry
+        inputTextField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                inputTextField.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+
+        inputTextSubmitButton = new JButton("Submit");
+        inputTextSubmitButton.setBackground(Color.black);
+        inputTextSubmitButton.setForeground(Color.lightGray);
+        inputTextSubmitButton.setFocusPainted(false);
+        inputTextSubmitButton.setFont(Awakening_Font.buttonSelectionFont());
+        inputTextSubmitButton.addActionListener(e -> {
+            String input = inputTextField.getText();
+            List<String> command = textParser.parseInput(input);
+            if (!command.get(0).equals("invalid")){
+                if (command.get(0).equalsIgnoreCase("help")) {
+                    //showImagePage("resources/images/help.PNG", "Return");
+                }
+                // add quit logic
+                else if (command.get(0).equalsIgnoreCase("quit")) {
+                    // TODO: exit page before exiting the program
+                    System.exit(0);
+                }
+                else{
+                    gameClassLoad.executeCommand(command);
+                }
+            }
+        });
+
+        inputTextPanel.add(inputTextField);
+        inputTextPanel.add(inputTextSubmitButton);
+
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 3;
+        directionalPanel.add(inputTextPanel, constraints);
 
         layoutManager.addGB(directionalPanel, 4, 4, 2, 2, .8, .3);
     }
 
     public static void populateMapButtonsGrid() {
-        layoutManager.addGB(new JButton("Map w/ Game Option Buttons"), 4, 0, 3, 2, .3, .1);
+        layoutManager.addGB(new JButton("Map w/ Game Option Buttons"), 4, 0, 2, 2, .3, .1);
     }
 
 //    public static void showImagePage(String imagePath, String buttonName) {
