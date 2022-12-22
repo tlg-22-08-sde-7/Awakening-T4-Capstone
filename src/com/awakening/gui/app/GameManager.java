5d3b36307ca5ffaea9629eda029e9c2189f27253
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2022-12-22 09:26:18 -0800

    AB#1041, AB#660: Unused import

--- src/com/awakening/gui/app/GameManager.java
+++ src/com/awakening/gui/app/GameManager.java
@@ -4,7 +4,6 @@ import com.awakening.app.Game;
 import com.awakening.app.TextParser;
 import com.awakening.app.UI;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
